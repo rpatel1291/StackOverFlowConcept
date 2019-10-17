b21d4e59bf81163e61bdef03e5553e79bd7c6c83
rpatel1291 <rpatel1291@gmail.com>
2019-10-17 16:21:37 -0400

    Update RegistrationServiceImp.java

--- src/main/java/com/pnctraining/service/RegistrationServiceImp.java
+++ src/main/java/com/pnctraining/service/RegistrationServiceImp.java
@@ -1,5 +1,6 @@
 package com.pnctraining.service;
 
+import com.pnctraining.entity.RegisterModel;
 import com.pnctraining.entity.UserEntity;
 import com.pnctraining.exception.CPSOException;
 import com.pnctraining.repository.UserRepository;
@@ -19,17 +20,23 @@ public class RegistrationServiceImp implements RegistrationService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+
+
     //Log4j
     private static final Logger LOGGER = LogManager.getLogger(RegistrationServiceImp.class);
 
     @Override
-    public void createNewUser(UserEntity userEntity) throws CPSOException, NoSuchAlgorithmException {
+    public void createNewUser(RegisterModel registerModel) throws NoSuchAlgorithmException {
         LOGGER.info("CREATE NEW USER: Starting process for creating new user.");
+        UserEntity userEntity = createUserEntityFromModel(registerModel);
         if(userRepository.findByEmail(userEntity.getEmail()) == null){
             if(userRepository.findByDisplayName(userEntity.getDisplayName()) == null){
                 userEntity.setPassword(toHexString(getSHA(userEntity.getPassword())));
-                userRepository.save(userEntity);
                 LOGGER.info(String.format("CREATE NEW USER: New user %s has been made", userEntity.getDisplayName()));
+                userRepository.save(userEntity);
             }
             else{
                 LOGGER.info("CREATE NEW USER: Exception display name chosen is already taken");
@@ -42,6 +49,14 @@ public class RegistrationServiceImp implements RegistrationService {
         }
     }
 
+    private UserEntity createUserEntityFromModel(RegisterModel registerModel) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(String.valueOf(sequenceGeneratorService.generateSequence(UserEntity.SEQUENCE_NAME)));
+        userEntity.setEmail(registerModel.getEmail());
+        userEntity.setPassword(registerModel.getPassword());
+        userEntity.setDisplayName(registerModel.getDisplayName());
+        return userEntity;
+    }
 
 
     private static byte[] getSHA(String input) throws NoSuchAlgorithmException {
